5b5149c3529afb53d7d624408fb556452bb32a2a
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-06-05 06:08:46 +0100

    added login,register request validation

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required',
+            'email.email' => 'Please Provide a valid Email Address',
+            'password' => 'Password is Required ',
+        ];
+    }
+
+    /*handle failed validation*/
+
+    public $validator = null;
+    protected function failedValidation(
+        \Illuminate\Contracts\Validation\Validator $validator
+    ) {
+        $this->validator = $validator;
+    }
+}
