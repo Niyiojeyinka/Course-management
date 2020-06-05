a085233f98c4dc9285bf10ad4591074f83239fcd
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-06-05 03:41:04 +0100

    added and installed jwt auth library

--- config/auth.php
+++ config/auth.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Authentication Defaults
@@ -14,7 +13,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -42,7 +41,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
             'hash' => false,
         ],
@@ -99,5 +98,4 @@ return [
             'expire' => 60,
         ],
     ],
-
 ];
